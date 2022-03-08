c36192111fabbc4f039c65d00d1b35d301df8191
Lourdes Rohan <lourdes.rohan@yale.edu>
2022-03-07 23:35:06 -0500

    keyboard instrument project

---
+++ exercises/keyboard_instrument/kimain.js
@@ -0,0 +1,90 @@
+// this is where you write your javascript
+
+$(window).keydown(function(e){
+
+  var key = e.key;
+
+  console.log(key);
+
+  if (key == "a") {
+    $(".message").html("askfm");
+  }
+  else if (key == "b") {
+    $(".message").html("bratz");
+  }
+  else if (key == "c") {
+    $(".message").html("carly rae jepsen");
+  } 
+  else if (key == "d") {
+    $(".message").html("dance dance revolution");
+  }
+  else if (key == "e") {
+    $(".message").html("edm");
+  }
+  else if (key == "f") {
+    $(".message").html("french toast");
+  }
+  else if (key == "g") {
+    $(".message").html("game of thrones");
+  }
+  else if (key == "h") {
+    $(".message").html("high school musical");
+  }
+  else if (key == "i") {
+    $(".message").html("ireland");
+  }
+  else if (key == "j") {
+    $(".message").html("justin bieber");
+  }
+  else if (key == "k") {
+    $(".message").html("kim there's people that are dying");
+  }
+  else if (key == "l") {
+    $(".message").html("lore dez");
+  }
+  else if (key == "m") {
+    $(".message").html("multiple choice exam");
+  }
+  else if (key == "n") {
+    $(".message").html("neverland");
+  }
+  else if (key == "o") {
+    $(".message").html("oreo cereal");
+  }
+  else if (key == "p") {
+    $(".message").html("piano lessons");
+  }
+  else if (key == "q") {
+    $(".message").html("quizzes");
+  }
+  else if (key == "r") {
+    $(".message").html("rotc");
+  }
+  else if (key == "s") {
+    $(".message").html("selena gomez");
+  }
+  else if (key == "t") {
+    $(".message").html("tiny handwriting");
+  }
+  else if (key == "u") {
+    $(".message").html("underclassmen");
+  }
+  else if (key == "v") {
+    $(".message").html("velvet");
+  }
+  else if (key == "w") {
+    $(".message").html("watermelon bubblegum");
+  }
+  else if (key == "x") {
+    $(".message").html("xmas");
+  }
+  else if (key == "y") {
+    $(".message").html("yolo");
+  }
+  else if (key == "z") {
+    $(".message").html("zookeeper");
+  }
+});
+
+
+
